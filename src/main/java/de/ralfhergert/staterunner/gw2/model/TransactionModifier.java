6cc06d4723d78d7432d357d9783a1b91d162c121
Ralf Hergert <Ralf.Hergert@haufe-lexware.com>
2022-11-07 16:58:24 +0100

    removed unused import statements

--- src/main/java/de/ralfhergert/staterunner/gw2/model/TransactionModifier.java
+++ src/main/java/de/ralfhergert/staterunner/gw2/model/TransactionModifier.java
@@ -1,15 +1,9 @@
 package de.ralfhergert.staterunner.gw2.model;
 
-import de.ralfhergert.jaxb.adapers.DurationAdapter;
 import de.ralfhergert.staterunner.gw2.model.transaction.SRTransaction;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlAttribute;
-import jakarta.xml.bind.annotation.XmlElementWrapper;
-import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-
-import java.time.Duration;
-import java.util.List;
 
 /**
  * TransactionModifiers are used to manipulate a transaction before it is
